bb111ebad3710744ee3fc19ff8238a5eb1f53ad5
Muhammad Adityo Fathur Rahim <m.adityo.rahim@wgs-id.com>
2024-02-20 13:00:38 +0700

    Feat: add saving photos feature

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -11,7 +11,9 @@ export class PhotoService {
 
   constructor() { }
 
-  public async addNewToGallery() {
+  // Old Method that just saving on the apps
+  // so the photos will deleted when the apps is closed/refreshed
+  public async addNewToGalleryOld() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -24,6 +26,59 @@ export class PhotoService {
       webviewPath: capturedPhoto.webPath!
     });
   }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, // automatically take a new photo with the camera
+      quality: 100 // highest quality (0 to 100)
+    });
+    console.log(capturedPhoto);
+
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    console.log(savedImageFile);
+
+    this.photos.unshift(savedImageFile);
+  }
+
+  private async savePicture(photo: Photo) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+
+    // Write the file to the data directory
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 export interface UserPhoto {
